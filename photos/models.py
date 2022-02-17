00e66daa6326e9da15e46b7f732d7340f87b6d26
samandar_developer1 <90138640+samandar-shoyimov@users.noreply.github.com>
2022-02-17 11:59:23 +0500

    added staticfiles

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,4 @@
+from distutils.command.upload import upload
 from django.db import models
 from PIL import Image
 
@@ -8,9 +9,12 @@ class Category(models.Model):
     return self.name
   
 class Photo(models.Model):
-  category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
+  category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)
   image = models.ImageField(null=False, blank=False)
   description = models.TextField()
+  
+  def save(self, *args, **kwargs) -> None:
+      return super().save(*args, **kwargs)
   
   def __str__(self):
     return self.description
--- photos/views.py
+++ photos/views.py
@@ -1,13 +1,29 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import redirect, render
+from .models import Category, Photo
+from .forms import PhotoForm
+from django.contrib import messages
 
 def gallery(request):
-  return render(request, 'photos/gallery.html')
+  categories = Category.objects.all()
+  photos = Photo.objects.all()
+  
+  context = {'categories':categories, 'photos':photos}
+  return render(request, 'photos/gallery.html', context)
 
 def viewPhoto(request, pk):
-  return render(request, 'photos/photo.html')
+  photo = Photo.objects.get(id=pk)
+  context = {'photo':photo}
+  return render(request, 'photos/photo.html', context)
 
 def addPhoto(request):
-  return render(request, 'photos/add.html')
+  form = PhotoForm()
+  if request.method == 'POST':
+    form = PhotoForm(request.POST)
+    if form.is_valid():
+      form.save()
+      messages.success(request, 'You uploaded picture')
+      return redirect('/')
+  
+  context = {'form':form}
+  return render(request, 'photos/add.html', context)
 
